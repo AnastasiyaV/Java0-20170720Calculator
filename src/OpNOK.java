ff3bc213ebdb641b522eb68a689ae72e2b963453
ADMINRG-A83SOKV\user <anastasiya.vynogradska@gmail.com>
2017-09-21 17:10:11 +0300

    я списала реализацию метода

--- src/OpNOK.java
+++ src/OpNOK.java
@@ -1,63 +1,51 @@
-import java.util.ArrayList;
-import java.util.List;
+public class OpNOK implements Operation {
 
-public class OpNOK implements Operation{
-
-    List<Integer> simpleDividersFirstNumber = new ArrayList<Integer>();
-    List<Integer> simpleDividersSecondNumber = new ArrayList<Integer>();
-    List<Integer> unicSimpleDividers = new ArrayList<Integer>();
+    int gcd(int a, int b){
+        return b == 0 ? a : gcd(b,a % b);
+    }
     @Override
     public int exec(int a, int b) {
-//        simpleDividersFirstNumber.add(a);
+        return a / gcd(a, b) * b;
+    }
+
+//    List<Integer> simpleDividersFirstNumber = new ArrayList();
+//    List<Integer> simpleDividersSecondNumber = new ArrayList();
+//    List<Integer> unicSimpleDividers = new ArrayList();
+//        double sqrt = Math.sqrt(a);
+//        int currentValue = a;
+//        int multiplier = 2;
+//        while (currentValue > 1 && multiplier <= sqrt)
+//        {
+//            if (currentValue % multiplier == 0)
+//            {
+//                System.out.print("multiplier = "+multiplier + " ");
+//                simpleDividersFirstNumber.add(multiplier);
+//                currentValue /= multiplier;
+//            }
+//            else {
+//                if (multiplier == 2) {
+//                    multiplier++;
+//                } else {
+//                    multiplier += 2;
+//                }
+//            }
+//        }
+//        if (currentValue != 1)
+//        {
+//            System.out.print(currentValue);
+//        }
+//        return currentValue;
+
 //        for(int i=0;i<simpleDividersFirstNumber.size();i++){
 //            System.out.println(simpleDividersFirstNumber.get(i));
+//        System.out.println(simpleDividersFirstNumber);
+//        for (int currentSimpleDividersFirstNumber: simpleDividersFirstNumber)
+//            System.out.println(currentSimpleDividersFirstNumber);
 
+
+    //Greatest common divider:
 //        if (b == 0) {
 //            return Math.abs(a);
 //        }
 //        return exec(b, a % b);
-
-//        A recursive method would be:
-//        static int gcd(int a, int b)
-//        {
-//            if(a == 0 || b == 0) return a+b; // base case
-//            return gcd(b,a%b);
-//        }
-//        Using a while loop:
-//        static int gcd(int a, int b)
-//        {
-//            while(a!=0 && b!=0) // until either one of them is 0
-//            {
-//                int c = b;
-//                b = a%b;
-//                a = c;
-//            }
-//            return a+b; // either one is 0, so return the non-zero value
-//        }
-
-        double sqrt = Math.sqrt(a);
-        int currentValue = a;
-        int multiplier = 2;
-        while (currentValue > 1 && multiplier <= sqrt)
-        {
-            if (currentValue % multiplier == 0)
-            {
-                System.out.print(multiplier + " ");
-                currentValue /= multiplier;
-            }
-            else if (multiplier == 2)
-            {
-                multiplier++;
-            }
-            else
-            {
-                multiplier += 2;
-            }
-        }
-        if (currentValue != 1)
-        {
-            System.out.print(currentValue);
-        }
-        return currentValue;
-    }
 }
